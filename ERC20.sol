af65565ecae024d0f209d46a0e597fac37b8e747
DigiCris <cmarchesetdiii@gmail.com>
2025-06-16 18:59:51 -0300

    feat+fix/ICO+transferFrom/cmarchese: Agregué la ICO y arreglé explicando el porque no había funcionado bien en clase

--- ERC20.sol
+++ ERC20.sol
@@ -9,7 +9,9 @@ abstract contract ERC20 is IERC20 {
     uint8 internal _decimals;
     uint256 internal _totalSupply;
     mapping (address=>uint256) internal _balanceOf;
-    mapping (address => mapping (address => uint256)) internal _allowance;
+    mapping (address => mapping (address => uint256)) public _allowance;
+    uint256 public logAllowance;
+
 
     constructor(string memory name_,string memory symbol_) {
         _name = name_;
@@ -59,8 +61,22 @@ abstract contract ERC20 is IERC20 {
         return true;
     }
 
+    /*
+        El p roblema de la clase pasada era en 
+        require(_allowance[_from][msg.sender] > 0...)
+        debido a que estabamos haciendo un approve al contrato pero como haciamos el sell con el mismo
+        contrato, el msg.sender era mi EOA y no el contrato el cual habíamos aprobado y por lo tanto
+        nos quedaba 0>0 y logicamente esto revertía. Solución, acá puede ser llamar directamente a
+        _transfer(from, to, value) ya que es interno.
+
+        Luego habían otrod dos errores.El require debía comparar:
+        _allowance[_from][msg.sender] >= _value  y no contra 0 y
+
+        _allowance[_from][msg.sender] -= _value; // para decrementarlo cada vez que alguien usa el allowance
+    */
     function transferFrom(address _from, address _to, uint256 _value) public  returns (bool success) {
-        require(_allowance[_from][msg.sender] > 0, "InsuficientAllowance");
+        require(_allowance[_from][msg.sender] >= _value, "InsuficientAllowance"); // modificado fuera de la clase
+        _allowance[_from][msg.sender] -= _value; // agregado fuera de la clase
         return _transfer(_from, _to, _value);
     }
 
--- Token.sol
+++ Token.sol
@@ -12,7 +12,7 @@ contract Token is ERC20 {
         _;
     }
 
-    constructor(string memory symbol_) ERC20("ETH KIP Token", symbol_) {
+    constructor() ERC20("ETH KIP Token", "EKT") {
         _mint(msg.sender, 1_000_000 * (10**decimals()));
         owner = msg.sender;
     }
@@ -20,4 +20,25 @@ contract Token is ERC20 {
     function mint(address to, uint256 value)  public onlyOwner {
         _mint(to, value);
     }
+
+    function buy() public payable {
+        _mint(msg.sender, msg.value);
+    }
+
+    /*
+        usa _transfer porque es una transferencia interna, por lo que
+        no tiene sentido darle un approve. Esto si es necesario cuando
+        el contrato es externo como en el caso de:
+        https://github.com/DigiCris/alephSolidity/tree/main/ICO/Contracts
+
+        La clase pasada fallaba porque le estabamos dando permiso al contrato
+        del token para movernos los tokens, pero el que lo estaba moviendo
+        era quien llamaba la función (el msg.sender) y por eso no pasaba
+        el require de la funcion transferFrom del ERC20
+    */
+    function sell(uint256 amount) public payable {
+        _transfer(msg.sender, address(this), amount);
+        payable(msg.sender).transfer(amount);
+    }
+
 }
